a960a412ec7bc9da01ea066a7420e3ec8e62bb1a
fromscode <chakrabortyshubham00@gmail.com>
2025-08-01 13:17:19 +0000

    feat: create book constructor and library array

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const myLibrary = [];
+
+function Book(title, author, pages, haveRead) {
+    if (!new.target) {
+        throw Error("Use new with constructor");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+    this.info = function() {
+        let output = this.title + " by " + this.author + ", " + this.pages + ", ";
+        if (haveRead) {
+            return output + "read";
+        }
+        return output + "not read yet";
+    };
+}
+
+function addBookToLibrary(title, author, pages, haveRead) {
+    const book = new Book(title, author, pages, haveRead);
+    myLibrary.push(book);
+}
