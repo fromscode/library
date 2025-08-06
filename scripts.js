21996c3d3c46019079f6debe3d6ddd3012cd7ad2
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-08-06 04:49:10 +0000

    create remove book button

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(title, author, pages, haveRead) {
     if (!new.target) {
         throw Error("Use new with constructor");
     }
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.haveRead = haveRead;
-    this.info = function() {
+    this.info = function () {
         let output = this.title + " by " + this.author + ", " + this.pages + ", ";
         if (haveRead) {
             return output + "read";
@@ -26,19 +27,33 @@ addBookToLibrary("test", "test", 10, true);
 addBookToLibrary("test2", "test2", 10, false);
 addBookToLibrary("test3", "test3", 10, true);
 
+const tBody = document.querySelector("tbody");
+
 function displayBook() {
+    tBody.textContent = null;
     myLibrary.forEach((book) => {
         showBook(book);
     });
+
+
+    const removeButtonAll = document.querySelectorAll("tr button");
+
+    removeButtonAll.forEach(button => {
+        button.addEventListener('click', () => {
+            const bookId = button.dataset.id;
+            console.log(bookId);
+            myLibrary = myLibrary.filter((book) => book.id != bookId);
+            displayBook();
+        });
+    });
 };
 
-const tBody = document.querySelector("tbody");
 
 function showBook(book) {
     const tr = document.createElement("tr");
     const title = document.createElement("td");
     title.textContent = book.title;
-    
+
     const author = document.createElement("td");
     author.textContent = book.author;
     const pages = document.createElement("td");
@@ -50,10 +65,15 @@ function showBook(book) {
     else {
         haveRead.textContent = "NO";
     }
+    const removeButton = document.createElement("button");
+    removeButton.type = "button";
+    removeButton.textContent = "Remove";
+    removeButton.dataset.id = book.id;
     tr.append(title);
     tr.append(author);
     tr.append(pages);
     tr.append(haveRead);
+    tr.append(removeButton)
 
     tBody.append(tr);
 }
@@ -74,9 +94,9 @@ formSubmitButton.addEventListener('click', () => {
     const author = document.querySelector("#author");
     const title = document.querySelector("#title");
     const pages = document.querySelector("#pages");
-    
+
     const read = document.querySelector('input[name="read"]:checked');
-    const readStatus  = read.value === "true" ? true : false;
+    const readStatus = read.value === "true" ? true : false;
 
     addBookToLibrary(title.value, author.value, pages.value, readStatus);
 
